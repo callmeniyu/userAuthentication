fe4d4b08fd47699ff8631d88aa9abb1b4e095600
callmeniyu <getniyashere@gmail.com>
2023-08-18 11:48:53 +0530

    hashing using bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const ejs = require('ejs');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use(express.static("public"));
 
 mongoose.connect('mongodb://127.0.0.1:27017/userDB');
 
-const userSchema =new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
@@ -35,38 +36,50 @@ app.get("/register", (req, res) => {
     res.render("register")
 });
 
-app.post("/register", async (req, res) => {
-    try {
-        const newUser = new User({
-            email: req.body.username,
-            password: md5(req.body.password)
-        });
+app.post("/register", (req, res) => {
 
-        await newUser.save();
-        console.log("Successfully Saved new User");
+    bcrypt.hash(req.body.password, saltRounds, async function (err, hash) {
+        if (err) {
+            console.log(err)
+        }
+        try {
+            const newUser = new User({
+                email: req.body.username,
+                password: hash
+            });
 
-        res.render("secrets");
-    } catch (err) {
-        console.log(err);
-        res.status(500).send("Error occurred while saving the user.");
-    }
+
+            await newUser.save();
+            console.log("Successfully Saved new User");
+
+            res.render("secrets");
+        } catch (err) {
+            console.log(err);
+            res.status(500).send("Error occurred while saving the user.");
+        }
+    });
+
+
 });
- 
+
 
 app.post("/login", (req, res) => {
     const userName = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({ email: userName })
         .then((doc) => {
             if (!doc) {
                 console.log("No such user found")
             } else {
-                if (doc.password === password) {
-                    res.render("secrets")
-                } else {
-                    console.log("Wrong password")
-                }
+                bcrypt.compare(password, doc.password, function (err, result) {
+                    if (result == true) {
+                        res.render("secrets");
+                    } else {
+                        console.log("Wrong password")
+                    }
+
+                });
             }
         })
         .catch((err) => {
